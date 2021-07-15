f735d51d13c4007f975140982a6b4118e7ba4830
rion lukas <rionlukas13@gmail.com>
2021-07-16 00:07:28 +0700

    Stock, Pembelian, Acc Order beres

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -45,6 +45,8 @@
               <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Order</a>
 
               <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">EOQ</a>
+              
+              <a href="{{ route('pembelian_acc') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">ACC Order & Pembelian</a>
             </div>
           </div>
         </div>
