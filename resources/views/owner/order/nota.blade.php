8bdc71cd9cbc20232c3fbe47a0ebc9d2ca83f6ee
rion lukas <rionlukas13@gmail.com>
2021-08-18 23:19:34 +0700

    nota

--- resources/views/owner/order/nota.blade.php
+++ resources/views/owner/order/nota.blade.php
@@ -1,66 +1,83 @@
-@extends('layouts.menu')
+v<head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@section('section_menu')
-    @parent
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 100%;
+                }
 
-@endsection
+                th, td {
+                text-align: left;
+                padding: 8px;
+                }
 
-@section('content')
+                tr:nth-child(even) {background-color: #f2f2f2;
+            }
+</style>
+        </style>
 
-<h1><u> Nota Pembayaran </u></h1>
+<h1><u>Nota Order</u></h1>
 
-<div class="mb-3">
-    <label for="inputOrderID" class="form-label">Order ID</label>
-    <input type="text" class="form-control" id="inputOrderId" name="OrderId" value={{$order[0]->OrderId}}>
-  </div>
-  <div class="mb-3">
-    <label for="inputNamaCustomer" class="form-label">Nama Customer</label>
-    <input type="text" class="form-control" id="inputNamaCustomer" name="NamaCustomer" value={{$order[0]->NamaCustomer}}>
-  </div>
+<table>
+    <table>
+        <tr>   
+            @foreach ($orders as $order)
+            <th></th> 
+                <th></th>
+                <th></th>
+                <th>Kepada Yth : </th>
+                <th>{{ $order->NamaCustomer }}</th>
+            </tr>                  
+            <tr>
+                <th>Order Id : </th>
+                <th>{{ $order->OrderId }}</th>
+                <th></th>
+                <th></th>
+                <th>{{ $order->Alamat }}</th>
+            </tr>
+            <tr>
+                <th>Tanggal : </th>
+                <th>{{ $order->Tanggal }}</th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
 
-  <div class="mb-3">
-    <label for="inputKodeKain" class="form-label">Kode Kain</label>
-    <input type="text" class="form-control" id="inputKodeKain" name="KodeKain" value={{$order[0]->KodeKain}}>
-  </div>
+            <tr>
+                <th>Nama Kain : </th>
+                <th>{{ $order->NamaKain }}</th>
+                <th></th>
+                <th>Jumlah Dalam Rol</th>
+                <th></th>
+            </tr>
 
-  <div class="mb-3" hidden=true id="div_namaKain">
-    <label for="inputNamaKain" class="form-label">Nama Kain</label>
-    <input type="text" class="form-control" id="inputNamaKain" name="NamaKain" value={{$order[0]->NamaKain}}>
-  </div>
+            <tr>
+                <th></th>
+                <th>{{ $order->NamaKain }}</th>
+                <th></th>
+                <th>{{ $order->Jumlah }}</th>
+                <th></th>
+            </tr>
 
-  <div class="mb-3">
-    <label for="inputJenisKain" class="form-label">Jenis Kain</label>
-    <input type="text" class="form-control" id="inputJenisKain" name="JenisKain" value={{$order[0]->JenisKain}}>
-  </div>
-
-  <div class="mb-3">
-    <label for="inputJumlahKain" class="form-label">Jumlah Kain Dalam Roll</label>
-    <input type="number" class="form-control" id="inputJumlahKain" name="Jumlah" value={{$order[0]->Jumlah}}>
-  </div>
-
-  <div class="mb-3">
-    <label for="inputHarga" class="form-label">Harga</label>
-    <input type="number" class="form-control" id="inputHarga" name="Harga" value={{$order[0]->Harga}}>
-  </div>
-
-  <div class="mb-3">
-    <label for="inputTotalHarga" class="form-label">Total Harga</label>
-    <input type="number" class="form-control" id="inputTotalHarga" name="TotalHarga" value={{$order[0]->TotalHarga}}>
-  </div>
-
-  <div class="mb-1">
-    <label for="inputTanggal" class="form-label">Tanggal Beli</label>
-    <input type="date" class="form-control" id="inputTanggal" name="Tanggal" value={{$order[0]->TanggalOrder}}>
-  </div>
-
-  <div class="mb-3">
-    <label for="inputKeterangan" class="form-label">Keterangan</label>
-    <input type="textarea" class="form-control" id="inputKeterangan" name="Keterangan" value={{$order[0]->Keterangan}}>
-  </div>
-
-  <div class="mb-3" hidden="true">
-    <label for="inputStatus" class="form-label">Status</label>
-    <input type="text" class="form-control" id="inputStatus" name="Status" value="Belum Disetujui" value={{$order[0]->Status}}>
-  </div>
-        
-@endsection
+ 
+            <tr>
+                <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            <tr>
+                    <td></td>
+                    <td></td>
+                    <td>Total : </td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                @endforeach
+                
+            </table>
+            
